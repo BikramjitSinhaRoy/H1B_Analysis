1b8afec2b327d7d46ad7356b800570683e603947
BikramjitSinhaRoy <bikramjitsinharoy10@gmail.com>
2023-12-11 19:36:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ state_count = h1b[h1b['CASE_STATUS']=='Certified'].groupby(['WORKSITE_STATE','st
 state_count['count_per_10k'] = ((state_count['count']/state_count['popullation'])*10000).round(0)
 top_state_count = state_count.sort_values('count_per_10k', ascending=False).head(number_of_states)
 
-state_bar = alt.Chart(top_state_count).mark_bar().encode(
+state_bar = alt.Chart(top_state_count).mark_bar(color='orange').encode(
 alt.X('count_per_10k', title='Number of Applications per 10k people'),
 alt.Y('state_name', sort='-x', title='State'))
 
@@ -80,7 +80,25 @@ user_state = st.selectbox(
     'Select a State',
     (list_of_top_states))
 
-st.write('You selected:', user_state)
+# st.write('You selected:', user_state)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write(":red[Total Number of Employers]")
+    top_emp2 = h1b[(h1b['CASE_STATUS']=='Certified') & (h1b['state_name'] == user_state)].groupby(['EMPLOYER_NAME'])['CASE_STATUS'].count().reset_index(name='count')
+    count = len(top_emp2['EMPLOYER_NAME'].unique())
+    st.subheader(count) 
+
+with col2:
+    st.write(":red[Median Wage]")
+    state_count2 = h1b[h1b['CASE_STATUS']=='Certified'].groupby(['WORKSITE_STATE','state_name', 'popullation']).agg({'CASE_STATUS': 'size', 'PREVAILING_WAGE': 'median'}).rename(columns={'CASE_STATUS' : 'count', 'PREVAILING_WAGE': 'median_wage'}).reset_index()
+    state_count2['count_per_10k'] = ((state_count2['count']/state_count2['popullation'])*10000).round(0)
+    user_state_median_wage = state_count2.loc[state_count2['state_name'] == user_state, 'median_wage'].values[0]
+    st.subheader(f"${user_state_median_wage}")
+  
+
+
 
 # TOP CITIES
 h1b.loc[h1b.WORKSITE_STATE == 'DC', 'WORKSITE_CITY'] = "Washington"
